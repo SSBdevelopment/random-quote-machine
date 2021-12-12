7b854adac8b557500b12fe4c8fdeb2a5d7f8cd58
SketchyJelly <stephenbrown.business@gmail.com>
2021-12-12 12:37:19 +1100

    Added 3 quotes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import './styles.css';
 
 //function App() {
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      /*quote:
+      author:*/
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    //change the quote
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="quote">Random Quote</h1>
-      {/*<h1 quote={this.getQuote}>{quote}</h1>*/}
+      {/*<p quote={this.state.quote}>{quote}</p>*/}
+      {/*<p author={this.state.author}>{author}</p>*/}
       <header>
-        <button className="new-quote"/*onClick={this.handleClick}*/>New Quote</button>
+        <button className="new-quote" onClick={this.handleClick}>New Quote</button>
       </header>
       </div>
     );
---
+++ src/quotes.js
@@ -0,0 +1,17 @@
+const quotes = [
+    {
+        "quote": "The hardest choices require the strongest wills",
+        "author": "Thanos"
+    },
+    {
+        "quote": "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        "author": "Albert Einstein"
+    },
+    {
+        "quote": "People say nothing is impossible, but I do nothing every day",
+        "author": "A.A. Milne"
+    }
+]
+/*"quote": "",
+"author": "" */
+export default quotes;
