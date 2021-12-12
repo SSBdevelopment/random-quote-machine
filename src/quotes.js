10037e2ad8228dfdcd7dc702a0294676eae26256
SketchyJelly <stephenbrown.business@gmail.com>
2021-12-12 21:15:23 +1100

    Added More Quotes

--- src/quotes.js
+++ src/quotes.js
@@ -10,8 +10,96 @@ const quotes = [
     {
         "quote": "People say nothing is impossible, but I do nothing every day.",
         "author": "A.A. Milne"
-    }
+    },
+    {
+        "quote": "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        "author": "Nelson Mandela"
+    },
+    {
+        "quote": "The way to get started is to quit talking and begin doing.",
+        "author": "Walt Disney"
+    },
+    {
+        "quote": "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+        "author": "Steve Jobs"
+    },
+    {
+        "quote": "If life were predictable it would cease to be life, and be without flavor.",
+        "author": "Eleanor Roosevelt"
+    },
+    {
+        "quote": "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+        "author": "Oprah Winfrey"
+    },
+    {
+        "quote": "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        "author": "James Cameron"
+    },
+    {
+        "quote": "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+        "author": "Ralph Waldo Emerson"
+    },
+    {
+        "quote": "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+        "author": "Benjamin Franklin"
+    },
+    {
+        "quote": "In the end, it's not the years in your life that count. It's the life in your years.",
+        "author": "Abraham Lincoln"
+    },
+    {
+        "quote": "I didn't fail the test. I just found 100 ways to do it wrong.",
+        "author": "Benjamin Franklin"
+    },
+    {
+        "quote": "I like criticism. It makes you strong.",
+        "author": "LeBron James"
+    },
+    {
+        "quote": "We must use time as a tool, not as a couch.",
+        "author": "John F. Kennedy"
+    },
+    {
+        "quote": "Nothing is impossible. The word itself says ‘I’m possible!",
+        "author": "Audrey Hepburn"
+    },
+    {
+        "quote": "There is nothing impossible to they who will try.",
+        "author": "Alexander the Great"
+    },
+    {
+        "quote": "The bad news is time flies. The good news is you’re the pilot.",
+        "author": "Michael Altshuler"
+    },
+    {
+        "quote": "Keep your face always toward the sunshine, and shadows will fall behind you.",
+        "author": "Walt Whitman"
+    },
+    {
+        "quote": "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        "author": "Winston Churchill"
+    },
+    {
+        "quote": "In a gentle way, you can shake the world.",
+        "author": "Gandhi"
+    },
+    {
+        "quote": "Learning how to be still, to really be still and let life happen—that stillness becomes a radiance.",
+        "author": "Morgan Freeman"
+    },
+    {
+        "quote": "If my mind can conceive it, if my heart can believe it, then I can achieve it.",
+        "author": "Muhammad Ali"
+    },
+    {
+        "quote": "Clouds come floating into my life, no longer to carry rain or usher storm, but to add color to my sunset sky.",
+        "author": "Rabindranath Tagore"
+    },
+    {
+        "quote": "Out of the mountain of despair, a stone of hope.",
+        "author": "Martin Luther King, Jr."
+    },
+
 ]
-/*"quote": "",
-"author": "" */
+
 export default quotes;
