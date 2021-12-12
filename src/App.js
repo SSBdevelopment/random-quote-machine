9f58aed4b49a9e184c5e3463d449354abcd6019b
SketchyJelly <stephenbrown.business@gmail.com>
2021-12-12 12:08:46 +1100

    Added css page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { Component } from 'react'
 import './App.css';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+//function App() {
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="quote">Random Quote</h1>
+      {/*<h1 quote={this.getQuote}>{quote}</h1>*/}
+      </div>
+    );
+  }
 }
 
 export default App;
